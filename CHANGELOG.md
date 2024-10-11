79e1b2ed40d76d3ef2efbdeda0b38830fc01774b
Marek Kurańda <89488013+mjkuranda@users.noreply.github.com>
2024-10-11 18:32:59 +0200

    [1.13.3] Refreshing user session (#17)

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -3,6 +3,10 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
+## [1.13.3] - 2024-10-11
+### Fixed
+- [Marek Kurańda](https://github.com/mjkuranda): Refreshing user session.
+
 ## [1.13.2] - 2024-10-11
 ### Added
 - [Marek Kurańda](https://github.com/mjkuranda): Support for new dish types.
--- contexts/user.context.tsx
+++ contexts/user.context.tsx
@@ -1,9 +1,15 @@
 import { UserPermissions } from '@/types/api.types';
 import { CurrentUser } from '@/types/user.types';
+import { HOUR, MINUTE } from '@/constants/numbers';
+import { refreshUserTokens } from '@/api/api';
 
 const state = {
     user: { login: '' }
 };
+
+const refreshSessionCallback = () => refreshUserTokens();
+
+let refreshSessionInterval = setInterval(() => {}, HOUR);
 
 export function isLoggedIn(): boolean {
     return state.user.login !== '';
@@ -12,10 +18,12 @@ export function isLoggedIn(): boolean {
 export function loginUser(login: string, permissions: UserPermissions): void {
     const user: CurrentUser = { login, ...permissions };
     state.user = { ...user };
+    refreshSessionInterval = setInterval(refreshSessionCallback, 5 * MINUTE);
 }
 
 export function logoutUser(): void {
     state.user = { login: '' };
+    clearInterval(refreshSessionInterval);
 }
 
 export function getUser(): CurrentUser {
