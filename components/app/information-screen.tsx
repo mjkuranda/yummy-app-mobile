9509d90631bd4363102b0f2cd26a2b9057e5ccf5
mkuranda <kuranda.marek@gmail.com>
2024-10-24 13:40:07 +0200

    black overlay

--- components/app/information-screen.tsx
+++ components/app/information-screen.tsx
@@ -19,21 +19,28 @@ export function InformationScreen({ title, description, image, authorInfo, link 
 
     return (
         <ImageBackground style={styles.mainContainer} source={image} data-author-info={authorInfo}>
-            <TouchableOpacity onPress={onPress}>
-                <View style={styles.informationScreenBox}>
-                    <Heading level={3} style={styles.informationHeading}>{title}</Heading>
-                    <Text style={styles.informationDescription}>{description}</Text>
-                </View>
-            </TouchableOpacity>
+            <View style={styles.blackOverlay}>
+                <TouchableOpacity onPress={onPress}>
+                    <View style={styles.informationScreenBox}>
+                        <Heading level={3} style={styles.informationHeading}>{title}</Heading>
+                        <Text style={styles.informationDescription}>{description}</Text>
+                    </View>
+                </TouchableOpacity>
+            </View>
         </ImageBackground>
     );
 }
 
 const styles = StyleSheet.create({
     mainContainer: {
-        ...constantStyles.flexCenter,
         width: Dimensions.get('window').width,
         height: Dimensions.get('window').height
+    },
+    blackOverlay: {
+        ...constantStyles.flexCenter,
+        backgroundColor: '#4446',
+        width: '100%',
+        height: '100%'
     },
     informationScreenBox: {
         backgroundColor: blueDeFranceAlpha,
