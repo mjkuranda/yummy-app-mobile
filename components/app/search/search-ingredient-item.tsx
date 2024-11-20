51a4acf62aa84d2d1e7694c189df08a395abdbe0
mkuranda <kuranda.marek@gmail.com>
2024-11-20 13:05:14 +0100

    new ingredient list with images + new category

--- components/app/search/search-ingredient-item.tsx
+++ components/app/search/search-ingredient-item.tsx
@@ -1,8 +1,9 @@
-import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+import { Image, StyleSheet, Text, TouchableOpacity, View } from 'react-native';
 import React, { useEffect, useState } from 'react';
 import { IngredientCategoryLabels } from '@/types/ingredient-category';
 import { cooper } from '@/constants/colors';
 import { useSearchContext } from '@/contexts/search.context';
+import { ingredientImage } from '@/assets/assets';
 
 interface SearchIngredientItemProps {
     ingredient: IngredientCategoryLabels;
@@ -33,7 +34,24 @@ export function SearchIngredientItem({ ingredient, defaultChecked }: SearchIngre
         <View style={styles['search-ingredient-category__ingredient']} data-filter="ingredient">
             <TouchableOpacity style={styles.checkbox} onPress={onPress}>
                 <View style={[styles.selectCircle, selected && { backgroundColor: cooper }]}></View>
-                <Text style={styles.selectText}>{ingredient.pl}</Text>
+                <View style={styles.imageContainer}>
+                    {ingredient.imageUrl
+                        ? <Image source={{ uri: `https://img.spoonacular.com/ingredients_250x250/${ingredient.imageUrl}` }}
+                            alt="Ingredient image icon"
+                            width={24}
+                            height={24}
+                            borderRadius={50}
+                        />
+                        : <Image source={ingredientImage}
+                            alt="Generic ingredient image icon"
+                            style={{ width: 24, height: 24 }}
+                            data-a-href="https://www.flaticon.com/free-icons/ingredients"
+                            data-a-title="ingredients icons"
+                            data-a-text="Ingredients icons created by Flat Icons - Flaticon"
+                        />
+                    }
+                    <Text style={styles.selectText}>{ingredient.pl}</Text>
+                </View>
             </TouchableOpacity>
         </View>
     );
@@ -42,7 +60,8 @@ export function SearchIngredientItem({ ingredient, defaultChecked }: SearchIngre
 const styles = StyleSheet.create({
     'search-ingredient-category__ingredient': {},
     checkbox: {
-        flexDirection: 'row'
+        flexDirection: 'row',
+        marginVertical: 4
     },
     selectCircle: {
         width: 20,
@@ -51,8 +70,15 @@ const styles = StyleSheet.create({
         borderWidth: 2,
         borderColor: cooper
     },
+    imageContainer: {
+        justifyContent: 'center',
+        alignItems: 'center',
+        flexDirection: 'row',
+        marginLeft: 4,
+        position: 'relative',
+        bottom: 2
+    },
     selectText: {
-        marginLeft: 10,
-        marginBottom: 10
+        marginLeft: 10
     }
 });
